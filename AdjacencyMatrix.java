8986667c53829bfea2db1d015e4fe70896f34f8e
aitbenmoumen <a.aitbenmoumen@gmail.com>
2024-10-13 15:13:40 +0100

    complete

--- AdjacencyMatrix.java
+++ AdjacencyMatrix.java
@@ -2,14 +2,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AdjacencyMatrix {
-    public static void calculerDeg(int n , int[][] matrice){
+    public static void calculerDeg(int n, int[][] matrice) {
 
         int[][] deg = new int[n][2];
-        int sommeP=0,sommeM=0;
+        int sommeP = 0, sommeM = 0;
         for (int i = 0; i < n; i++) {
             int degP = 0, degM = 0;
-            for (int j = 0; j < n; j++) { 
-                  if (matrice[i][j] == 1) {
+            for (int j = 0; j < n; j++) {
+                if (matrice[i][j] == 1) {
                     degP++;
                 }
                 if (matrice[j][i] == 1) {
@@ -26,12 +26,14 @@ public class AdjacencyMatrix {
             }
             System.out.println();
         }
-        for(int i=0;i<n;i++){
-                sommeP+=deg[i][0];
-                sommeM+=deg[i][1];
+        for (int i = 0; i < n; i++) {
+            sommeP += deg[i][0];
+            sommeM += deg[i][1];
         }
-        System.out.println("Somme de Deg+ = "+sommeP+" et Deg- = "+sommeM+"\n--------------------------------------");
-    } 
+        System.out.println(
+                "Somme de Deg+ = " + sommeP + " et Deg- = " + sommeM + "\n--------------------------------------");
+    }
+
     public static void main(String[] args) {
 
         Random random = new Random();
@@ -40,26 +42,27 @@ public class AdjacencyMatrix {
 
         System.out.print("Type the number of nodes :");
         n = scan.nextInt();
-       // forçage des arcs 
+        // forçage des arcs
         while (true) {
-        System.out.print("Type the number of arcs :");
-        arc = scan.nextInt();
-            if(arc<Math.pow(n,2)){
+            System.out.print("Type the number of arcs :");
+            arc = scan.nextInt();
+            if (arc < Math.pow(n, 2)) {
                 break;
             }
         }
-        
-        // pour la matrice 
+
+        // pour la matrice
         int[][] matrice = new int[n][n];
         // pour calculer les deg+ et deg- afin d'assurer que le graphe est oriente
-        
+
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 matrice[i][j] = 0;
             }
         }
 
-        // pour assurer le nb des arcs via un compteur qui compte le nombre des 1 dans la matrice
+        // pour assurer le nb des arcs via un compteur qui compte le nombre des 1 dans
+        // la matrice
         while (counter < arc) {
             i = random.nextInt(n);
             j = random.nextInt(n);
@@ -69,24 +72,22 @@ public class AdjacencyMatrix {
             }
         }
 
-        
-        // afficher la matrice 
+        // afficher la matrice
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 System.out.print(" " + matrice[i][j] + " ");
             }
             System.out.println();
         }
-        
-        //pour calculer et remplir la matrice deg 
-        calculerDeg(n , matrice);
-        
+
+        // pour calculer et remplir la matrice deg
+        calculerDeg(n, matrice);
 
         // traduire les arcs
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
-                if(matrice[i][j]==1){
-                    System.out.println("Node "+i+" is connected to "+j);
+                if (matrice[i][j] == 1) {
+                    System.out.println("Node " + i + " is connected to " + j);
                 }
             }
             System.out.println("--------------------------------------");
