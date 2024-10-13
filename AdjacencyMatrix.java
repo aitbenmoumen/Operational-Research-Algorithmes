dfc1302d131386bc44808829e85aff6f81006bb6
aitbenmoumen <a.aitbenmoumen@gmail.com>
2024-10-13 11:59:13 +0100

    AdjacencyMatrix

--- AdjacencyMatrix.java
+++ AdjacencyMatrix.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class matrice {
+public class AdjacencyMatrix {
     public static void main(String[] args) {
 
         Random random = new Random();
